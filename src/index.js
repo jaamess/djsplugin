9cb8ab8216d56892542ec02e77aaceab41557e01
jaamess <39378514+jaamess@users.noreply.github.com>
2022-05-11 17:56:24 -0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { SapphireClient } from '@sapphire/framework';
 import { token } from '../config.json';
 
-const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const client = new SapphireClient({
+    intents: ['GUILDS', 'GUILD_MESSAGES'],
+    caseInsensitiveCommands: true,
+    defaultPrefix: '-',
+    disableMentionPrefix: true,
+});
 
 client.login(token);
